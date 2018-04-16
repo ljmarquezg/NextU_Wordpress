71ce90f440e7652c496cc3310f33d6723142f4b2
Luis Jose Marquez Gonzalez <ljmarquezg@gmail.com>
2018-04-16 09:52:00 -0400

    Final Project
    Evaluación Final Los CMS y el Sitio Web .Leer el archivo readme.html antes de iniciar el proyecto.

--- wp-content/plugins/woocommerce-extra-accounts-fields/public/js/front-custom.js
+++
@@ -1,8 +0,0 @@
-// add the pickup date for jquery
-jQuery('document').ready(function () {
-    jQuery('#reg_date_picker').datepicker({
-        changeMonth: true,
-        changeYear: true,
-        dateFormat: "yy-mm-dd",
-    });
-});
